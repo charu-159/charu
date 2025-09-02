87fc22e84ef735b6a54fee7d6cd6f21725097af9
charu-159 <charupatel159@gmail.com>
2025-09-02 13:23:34 +0530

    first

---
+++ loops/even.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    for(int i=0;i<=10;i++)
+    {
+        if(i%2==0)
+        {
+            printf("%d\n",i);
+             printf("even number\n");
+        }
+        else
+        {
+            printf("%d\n",i);
+            printf("Odd Number\n");
+        }
+    }
+return 0;
+}
---
+++ loops/sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int sum=0;
+    for(int i=0;i<=20;i++)
+    {
+        sum=sum+i;
+    }
+    printf("Sum is %d\n",sum);
+    return 0;
+
+}
