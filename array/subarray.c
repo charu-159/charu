7c3801bdd158ea81fcc3ae9f6b462957bf47c10f
charu-159 <charupatel159@gmail.com>
2025-09-19 19:30:45 +0530

    first

---
+++ array/subarray.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+     int arr[7]={1,2,3,5,6,5,8};
+     int k=3;
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int windowsum=0;
+    for(int i=0;i<k;i++){
+        for(int j=3)
+    }
---
+++ function/tempCodeRunnerFile.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int num(int a){
+    printf("%d",a);
+    return a;
+}
+int main(){
+    int a;
+    printf("enter the value of a");
+    scanf("%d",&a);
+    num(a);
+    if(a%2==0)
+    printf("it is even");
+    else
+    printf("it is odd");
+    return 0;
+    printf("charu");
+
+}
