b2adce8e897d3b2cbd5ffd531939fb7f0e5d42a6
charu-159 <charupatel159@gmail.com>
2025-09-25 12:56:30 +0530

    first

---
+++ lecture 18/first.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    //find the maximum sum sub array of size k
+    //array subarray n*(n+1)/2 total 15
+    //subarray find then max find
+     int arr[]={2,1,5,4,7,8,1,2};
+     int k=2;
+    int n=sizeof(arr);///sizeof(arr[0]);
+    int wsum=0;
+    for(int i=0;i<k;i++){
+      wsum=wsum+arr[i];
+    }
+    int maxsum;
+    maxsum=wsum;
+
+    for(int j=k; j<n;j++)
+    {
+     wsum=wsum+arr[j]-arr[j-k];
+     if(wsum>maxsum)
+     maxsum=wsum;
+    }
+       printf("%d", maxsum);
+       return 0;
+}
+
+    
