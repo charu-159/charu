ef94ecf11d1f6a5302108140caf49da0ef5c35c5
charu-159 <charupatel159@gmail.com>
2025-10-08 22:45:45 +0530

    first

--- lecture 21/frequency.c
+++ lecture 21/frequency.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
-    char name[]="hello";
-    int ln=strlen(name);
-    //frequency of each character
-    int freq[256] = {0}; // Initialize frequency array for all ASCII characters
-    for(int i=0; i<ln; i++){        
-        freq[(int)name[i]]++; // Increment the frequency of the character
-        
+#include <stdio.h>
+#include <string.h>
 
+int main()
+{
+    char str[] = "madam";
+    int freq[256] = {0};   
+    int ln = strlen(str);
 
-    
+    for (int i = 0; i < ln; i++) {
+        freq[str[i]]++; 
     }
+
+    for (int i = 0; i < 256; i++) {
+        if (freq[i] > 0) {
+            printf("%c: %d\n", i, freq[i]);
+        }
+    }
+
+    return 0;
 }
