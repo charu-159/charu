b2c8da882045ea91a3571c8718af065cd25ea259
charu-159 <charupatel159@gmail.com>
2025-10-27 23:30:05 +0530

    first

--- lecture 26/leetcode121.c
+++ lecture 26/leetcode121.c
@@ -1,5 +1,5 @@
 //best time to buy and sell stock
-#include<stdio.h>
+/*#include<stdio.h>
 int main(){
     int arr[6]={7,1,5,3,6,4};//index =day
     int n=sizeof(arr)/sizeof(arr[0]);
@@ -25,7 +25,32 @@ int main(){
     printf("profit is:%d",profit);
     return 0;
 
+    }*/
+
+    #include <stdio.h>
+int main(){
+
+ 
+ int arr[6]={7,1,5,3,6,4};
+ int minPrize=arr[0];
+ int maxP=0;
+ for(int i =1;i<6;i++){
+    if(arr[i]<minPrize){
+        minPrize=arr[i];
+
+    }else 
+    {
+       int profit=arr[i]-minPrize;
+       if(profit>maxP){
+        maxP=profit;
+       }
     }
+    
+
+ }
+
+ printf("%d",maxP);
+}
 
 
 
